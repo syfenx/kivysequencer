ea4fde69d0f5bc6a50a73a7e56cfee94faf21571
syfenx <syfenx@gmail.com>
2018-03-02 06:43:12 -0600

    test

---
+++ aengine.py
@@ -0,0 +1,63 @@
+# import pyo as p
+from pyo import *
+#s = Server(sr=44100, nchnls=2, buffersize=512, duplex=0).boot()
+# s = Server(duplex=0)
+# s.boot()
+# s.start()
+
+class AudioEngine(object):
+    def __init__(self):
+        # self.s = self.engine
+        # self.start()
+        self.sr = 44100
+        self.nchnls = 2
+        self.buffersize = 512
+        self.duplex = 0
+        self.engine = Server(sr=self.sr, nchnls=self.nchnls, buffersize=self.buffersize, duplex=self.duplex)
+        self.engine.setInOutDevice(4)
+    def start(self):
+        self.engine.boot()
+        self.engine.start()
+        print("Engine started - Sample Rate: {sr} - Channels: {chan} - Buffer Size: {buf} - Duplex: {dup}".format(sr=self.sr,chan=self.nchnls,buf=self.buffersize,dup=self.duplex))
+    def record(self):
+        self.engine.recordOptions(dur=100, filename="output_13.wav", fileformat=0, sampletype=1)
+        self.engine.recstart()
+
+    # def playsound(self, filename):
+    #     self.sf = SfPlayer(filename, mul=0.3)
+    #     self.sf.mix(2).out()
+    #     print("soundplayed")
+    def playsound(self, filename):
+        self.sf = SfPlayer(filename, mul=0.3).mix(2).out()
+        # self.sf = self.sf.mix(2).out()
+        print("soundplayed")
+
+class AudioItem(object):
+    def __init__(self, filename, volume, pan, vel):
+        self.filename = filename
+        self.volume = volume
+        self.pan = pan
+        self.fx = []
+        self.vel = vel
+        self.sf = SfPlayer(self.filename, mul=0.3).stop()
+        self.sf2 = self.sf.mix(2).out()
+    def play(self):
+        self.sf.play()
+        # print("played audioitem")
+        # self.a = Trig()
+        # self.env = HannTable()
+        # self.tenv = TrigEnv(self.sf, table=self.env, dur=5, mul=.3)
+        # self.tenv.out()
+        # self.n = Noise(self.tenv).out()
+    def setfn(self, path):
+        self.sf.setPath(path)
+
+class AudioMixer(object):
+    def __init__(self):
+        print("mixer started")
+        self.tracks = []
+
+    def addTrack(self, fn):
+        self.fn = fn 
+        self.tracks.append(AudioItem(self.fn, 0, 0, 0))
+        print("added {} to mixer, total tracks = {}".format(self.fn, len(self.tracks)))
